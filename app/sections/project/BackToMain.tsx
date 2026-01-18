bfbf3b8b4124eb8eff69b863c8fde793f8a9e6b8
jiyuunyang <jiyuunyang@gmail.com>
2026-01-19 00:04:56 +0900

    [Fix] fix BackToMain navigation

--- app/sections/project/BackToMain.tsx
+++ app/sections/project/BackToMain.tsx
@@ -1,21 +1,17 @@
 'use client';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function BackToMain() {
-  const router = useRouter();
   return (
     <div className='p-10 flex flex-row justify-center'>
-      <button
+      <Link
         className='w-40 flex flex-row items-center gap-3 hover:opacity-80 cursor-pointer'
-        onClick={() => router.back()}
+        href='/'
       >
         <ArrowLeft size={24} />
-        <Link className='text-xl font-bold' href='/'>
-          메인으로
-        </Link>
-      </button>
+        <span className='text-xl font-bold'>메인으로</span>
+      </Link>
     </div>
   );
 }
